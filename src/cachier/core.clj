40a0444f6b7b0e92588f9a92804bb06a5ff74f25
Mykhailo Kozik <misha@zoomdata.com>
2015-07-02 15:48:00 +0300

    Rewritten cache using clojure PersistentQueue and atom

--- src/cachier/core.clj
+++ src/cachier/core.clj
@@ -1,25 +1,60 @@
 (ns cachier.core)
 
-;; fill fn
-(def data-batch (ref (fn []
-                       (println "Batch started...")
-                       (Thread/sleep 10000)
-                       (println "Batch finished.")
-                       (range 10))))
+;; TODO remove fill fn
+(def data-batch (fn []
+                  (println "Batch started...")
+                  (Thread/sleep 10000)
+                  (println "Batch finished.")
+                  (range 10)))
 
-(def batch-in-progress (ref false))
-;; queue
-(def q (java.util.concurrent.LinkedBlockingQueue.))
+;; TODO make a fn
+(def cache
+  (atom {:data-batch-fn data-batch
+         :batches-in-progress 0
+         :queue (clojure.lang.PersistentQueue/EMPTY)
+         :cap 5}))
 
-(defn get-from-cache []
-  (dosync
-   ;; trigger cache fill whe cap is reached
-   (when (and (< (count q) 5) (not @batch-in-progress))
-     (future (dosync (doseq [d (data-batch)]
-                       (.put q d))
-                     (ref-set batch-in-progress false)))
-     (ref-set batch-in-progress true))
-   (if (pos? (count q))
-     (.take q)
-     :no-data
-     )))
+(defn- batch-start [cache]
+  (swap! cache
+         (fn [cache-map]
+           (update-in cache-map [:batches-in-progress] inc))))
+
+(defn- batch-stop [cache data]
+  (swap! cache
+         (fn [cache-map]
+           (-> cache-map
+               (update-in [:queue] (fn [q] (into q data)))
+               (update-in [:batches-in-progress] dec)))))
+
+
+(defn- fill-cache [cache]
+  (future
+    (println "fill-cache-start" cache)
+    (batch-start cache)
+    (println "fill-cache-after-start" cache)
+    (batch-stop cache ((:data-batch-fn @cache)))
+    (println "fill-cache-after-stop" cache)))
+
+(defn hit [cache]
+  (println "hit-start" cache)
+  (let [{:keys [data-batch-fn
+                batches-in-progress
+                queue
+                cap]} @cache]
+    ;; trigger cache fill when cap is reached
+    (println "hit-cap-check" cache)
+    (when (and (< (count queue) cap)
+               (<= batches-in-progress 0))
+      (println "hit-before-fill" cache)
+      (fill-cache cache)
+      (println "hit-after-fill" cache))
+    (println "before-get" cache)
+    (if (pos? (count queue))
+      (let [e (peek queue)]
+        (swap! cache (fn [cache-map]
+                       (-> cache-map
+                           (update-in [:queue] pop))))
+        (println "popped")
+        e)
+      :no-data
+      )))
