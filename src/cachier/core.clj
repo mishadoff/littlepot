f489191fac23f9ab23f7f73cd1324ad691341367
Mykhailo Kozik <misha@zoomdata.com>
2015-07-03 19:13:31 +0300

    Multiple consumers problem fixed using STM

--- src/cachier/core.clj
+++ src/cachier/core.clj
@@ -1,45 +1,57 @@
-(ns cachier.core)
+(ns cachier.core
+  (:import [java.util.concurrent.locks ReentrantReadWriteLock]))
 
 (defn make-cache [data-batch-fn]
-  (atom {:data-batch-fn data-batch-fn
-         :batches-in-progress 0
-         :batches-done 0
-         :queue (clojure.lang.PersistentQueue/EMPTY)
-         :cap 5}))
+  (ref {:data-batch-fn data-batch-fn
+        :batches-in-progress 0
+        :batches-done 0
+        :queue (clojure.lang.PersistentQueue/EMPTY)
+        :cap 5}))
 
-(defn- batch-start [cache]
-  (swap! cache
-         (fn [cache-map]
-           (update-in cache-map [:batches-in-progress] inc))))
+(defn cache-exhausted? [cache]
+  (let [{:keys [cap queue]} @cache]
+    (< (count queue) cap)))
 
-(defn- batch-stop [cache data]
-  (swap! cache
-         (fn [cache-map]
-           (-> cache-map
-               (update-in [:queue] (fn [q] (into q data)))
-               (update-in [:batches-in-progress] dec)
-               (update-in [:batches-done] inc)))))
+(defn active-batch? [cache]
+  (let [{:keys [batches-in-progress]} @cache]
+    (pos? batches-in-progress)))
+
 
 
 (defn- fill-cache [cache]
   (future
-    (batch-start cache)
-    (batch-stop cache ((:data-batch-fn @cache)))))
+    (let [data ((:data-batch-fn @cache))]
+      (dosync
+       (alter cache
+              (fn [cache-map]
+                (-> cache-map
+                    (update-in [:queue] (fn [q] (into q data)))
+                    (update-in [:batches-in-progress] dec)
+                    (update-in [:batches-done] inc))))))))
 
 (defn hit [cache]
-  (let [{:keys [data-batch-fn
-                batches-in-progress
-                queue
-                cap]} @cache]
-    ;; trigger cache fill when cap is reached
-    (when (and (< (count queue) cap)
-               (<= batches-in-progress 0))
-      (fill-cache cache))
-    (if (pos? (count queue))
-      (let [e (peek queue)]
-        (swap! cache (fn [cache-map]
-                       (-> cache-map
-                           (update-in [:queue] pop))))
-        e)
-      :no-data
-      )))
+  (dosync
+   (when (and (cache-exhausted? cache) 
+              (not (active-batch? cache)))
+     ;; mark progress batch
+     (alter cache
+            (fn [cache-map]
+              (update-in cache-map [:batches-in-progress] inc)))
+     ;; run background process to fill the data
+     (fill-cache cache))
+     (let [{:keys [data-batch-fn
+                   batches-in-progress
+                   queue
+                   cap]} @cache]
+       (if (not (empty? queue))
+         (let [e (peek queue)]
+           (alter cache (fn [cache-map]
+                          (-> cache-map
+                              (update-in [:queue] pop))))
+           e)
+         :no-data
+         ))))
+
+  
+        
+  
--- test/cachier/core_test.clj
+++ test/cachier/core_test.clj
@@ -16,15 +16,16 @@
     (is (every? #(= % :no-data) (take 100 (repeatedly #(hit cache)))))
     ))
 
-;; TODO test number in batch not more than
-
-
 ;; TODO failing when multiple consumers
 (deftest test-multiple-consumers
-  (let [number-of-consumers 2
+  (let [number-of-consumers 10
+        batches-active (atom 0)
         batch-data-fn (fn []
-                        (Thread/sleep 1000)
-                        (repeat 1000 1))
+                        (swap! batches-active inc)
+                        (Thread/sleep 100)
+                        (let [data (repeat 1000 1)]
+                          (swap! batches-active dec)
+                          data))
         cache (make-cache batch-data-fn)
         run (atom true)
         consumer (fn []
@@ -33,7 +34,7 @@
                      (loop [ones 0]
                        (if @run
                          (do
-                           ;; (println "running...")
+                           (Thread/sleep (rand-int 10))
                            (let [e (hit cache)]
                              (cond (= e 1) (recur (inc ones))
                                    (= e :no-data) (recur ones)
@@ -41,9 +42,16 @@
                          ones))))
         ;; start 100 consumers
         consumers (take number-of-consumers (repeatedly consumer))]
+    ;; track no more then 1 active batch
+    (add-watch batches-active :no-more-than-one
+               (fn [_ _ _ new-value]
+                 (when (> new-value 1) ;; max parallel batches
+                   (println "ERROR")
+                   (is false)
+                   (throw (IllegalArgumentException.)))))
     ;; kick-off computation
-    (count consumers)
-    ;; wait 5 seconds for consumers to work
+    (doall consumers)
+    ;; wait 10 seconds for consumers to work
     (Thread/sleep 10000)
     ;; request stop
     (reset! run false)
@@ -57,4 +65,5 @@
       (println consumer-ones left-in-cache total-produced)
       (is (pos? total-produced))
       (is (= (+ consumer-ones left-in-cache) total-produced))
+      (is (<= left-in-cache 1000))
       )))
